9831354e97f0eaed2434750a805b093911b0a0f2
DifexRV <roOmkin@gmail.com>
2022-11-06 21:22:30 +0700

    Рабочая ветка для ДЗ - Объекты и классы.

---
+++ src/Pro/sky/Book.java
@@ -0,0 +1,45 @@
+package Pro.sky;
+
+public class Book {
+    private final Author authorName;
+    private final String bookName;
+    private int publishingYear;
+
+    public Book(Author authorName, String bookName, int publishingYear) {
+        this.authorName = authorName;
+        this.bookName = bookName;
+        this.publishingYear = publishingYear;
+    }
+
+    public String getBookName() {
+        return this.bookName;
+    }
+
+    public int getPublishingYear() {
+        return this.publishingYear;
+    }
+
+    public void setPublishingYear(int publishingYear) {
+        if (publishingYear < 1950 || publishingYear > 2050) {
+            System.out.println("Invalid publishing year parameter: " + publishingYear);
+            return;
+        }
+        this.publishingYear = publishingYear;
+    }
+
+    public boolean equals(Object other) {
+        if (this.getClass() != other.getClass()) {
+            return false;
+        }
+        Book c2 = (Book) other;
+        return (getBookName()).equals(c2.getBookName());
+    }
+
+    public int hashCode() {
+        return java.util.Objects.hash(bookName);
+    }
+
+    public String toString() {
+        return " Название книги: " + getBookName() + ". Год издания: " + getPublishingYear();
+    }
+}
---
+++ src/Pro/sky/Main.java
@@ -0,0 +1,30 @@
+package Pro.sky;
+
+
+public class Main {
+
+
+    public static void main(String[] args) {
+
+        Author author = new Author("Steven", "King");
+        Book book = new Book(author, "11/22/63", 2011);
+
+        Author author1 = new Author("Fyodor", "Dostoevsky");
+        Book book1 = new Book(author1, "Demons", 2016);
+
+        Author author2 = new Author("Steven", "King");
+        Book book2 = new Book(author2, "11/22/63", 2011);
+
+
+        System.out.println("Автор: " + author + book);
+        System.out.println("Автор: " + author1 + book1);
+
+        System.out.println(author.equals(author1));
+        System.out.println(book.equals(book1));
+
+        System.out.println(author.equals(author2));
+        System.out.println(book.equals(book2));
+
+    }
+
+}
